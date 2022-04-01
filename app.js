1bb95f68d5932713c5ec4e09154cb7ff6c92ce7f
Blair Bristow <blairhbristow@gmail.com>
2022-04-01 17:12:18 -0400

    Edit

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ let acceptingInput = true
 
 const screen = document.querySelector("#screen")
 const spans = document.querySelectorAll(".buttons span")
-const spanArray = Array.from(spans)
+const calcButtons = Array.from(spans)
 document.querySelectorAll(".operator")[1].textContent = "/"
 document.querySelectorAll(".operator")[2].textContent = "*"
 
-spanArray.map(button => button.addEventListener("click", function () {
+calcButtons.map(button => button.addEventListener("click", function () {
     if (button.textContent !== "=" && acceptingInput) {
         screen.textContent += button.textContent
     } if (button.textContent === "C" && acceptingInput) {
