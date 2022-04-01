48657e1bd184db34a09dc8d2f479315ea4d3a906
Blair Bristow <blairhbristow@gmail.com>
2022-04-01 15:03:57 -0400

    Add error message capability

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-///https://medium.com/@blake.a.long/a-basic-dom-calculator-1303de2f8024
-///https://freshman.tech/calculator/
-
 document.addEventListener("DOMContentLoaded", function () {
     const screen = document.querySelector("#screen")
     const spans = document.querySelectorAll(".buttons span")
@@ -8,15 +5,25 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".operator")[1].innerText = "/"
     document.querySelectorAll(".operator")[2].innerText = "*"
 
-    spanArray.map(button => button.addEventListener("click", function() {
-        if(button.innerText !== "=") {
+    spanArray.map(button => button.addEventListener("click", function () {
+        if (button.innerText !== "=") {
             screen.innerText += button.innerText
-        }
-        if(button.innerText === "="){
-            screen.innerText = eval(screen.innerText)
-        }
-        if(button.innerText === "C"){
+        } if (button.innerText === "C") {
             screen.innerText = ""
+        } 
+        try {
+            if (button.innerText === "=") {
+                if (screen.innerText.includes("/0")){
+                    screen.innerText = "Error, Press C"
+                }
+                screen.innerText = eval(screen.innerText)
+            }
+        }
+        catch (error) {
+            screen.innerText = "Error, Press C"
+        }
+        if (screen.innerText == "Error, Press C"){
+            
         }
     }))
 })
